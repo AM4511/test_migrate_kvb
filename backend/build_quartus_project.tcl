f898dca607704e950e8af2de9649e75a5b0700c5
David Rauth <drauth@kns.com>
2019-03-13 19:27:07 -0400

    Modify build_quartus_project script to handle multiple revisions

--- backend/build_quartus_project.tcl
+++ backend/build_quartus_project.tcl
@@ -1,28 +1,49 @@
-# ##################################################################################
+################################################################################
 # File         : build_quartus_project.tcl
 # Description  : TCL script to compile design
-#
-# ##################################################################################
+################################################################################
 set myself [info script]
 puts "Running ${myself}"
 
 
-###################################################################################
+################################################################################
 # Load Quartus Prime Tcl packages
-###################################################################################
+################################################################################
 package require ::quartus::project
 package require ::quartus::flow
 
 
-###################################################################################
+################################################################################
 # Load the project setup environment variables
-###################################################################################
+################################################################################
 set MYSELF_PATH [ file dirname [ file normalize ${myself} ] ]
 source [file join ${MYSELF_PATH} "setup.tcl"]
 
 
-########################################################################################
-# Open and compile project
-########################################################################################
-project_open -current_revision [file join ${WORK_PATH} ${PROJECT_NAME}]
-execute_flow -compile
+################################################################################
+# Compile project revision(s)
+################################################################################
+set project_file [file join ${WORK_PATH} ${PROJECT_NAME}]
+set project_revisions [get_project_revisions $project_file]
+
+project_open -current_revision ${project_file}
+set BUILDID [get_parameter -name "BUILDID"]
+set rev_build_name "${PROJECT_NAME}_[lindex $quartus(args) 0]_build${BUILDID}"
+
+if {[lsearch [get_project_revisions ${project_file}] ${rev_build_name}] != -1} {
+    puts "Building ${rev_build_name}"
+
+    set_current_revision ${rev_build_name}
+    execute_flow -compile
+} else {
+    dict for {rev_name rev_info} ${REV_DATA} {
+        set rev_build_name "${PROJECT_NAME}_${rev_name}_build${BUILDID}"
+        puts "Building ${rev_build_name}"
+
+        set_current_revision ${rev_build_name}
+        execute_flow -compile
+    }
+}
+
+set_current_revision [lindex $project_revisions 0]
+project_close
